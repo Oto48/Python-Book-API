4a83a7a763c2d289e980b99bdaeb25be60f8805f
oto48 <otchokhonelidze4@gmail.com>
2025-06-06 00:37:41 +0400

    Implement Delete endpoints Remove __pycache__ and .pyc files and update .gitignore

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from typing import List
 from models import Book, BookCreate
+from uuid import UUID
+from fastapi import HTTPException
 
 app = FastAPI()
 
+# Starting data
 db: List[Book] = [
     Book(
         title="1984",
@@ -19,16 +22,28 @@ db: List[Book] = [
     )
 ]
 
+# Home page
 @app.get("/")
 async def read_root():
     return {"message": "Welcome to the Book Library API!"}
 
+# Get the list of Books
 @app.get("/api/books", response_model=List[Book])
 async def fetch_books():
     return db
 
+# Add a new Book
 @app.post("/api/books", response_model=Book)
 async def add_book(book_data: BookCreate):
     new_book = Book(**book_data.model_dump())
     db.append(new_book)
     return new_book
+
+# Delete a Book
+@app.delete("/api/books/{book_id}")
+async def delete_book(book_id: UUID):
+    for index, book in enumerate(db):
+        if book.id == book_id:
+            del db[index]
+            return {"message": "Book deleted successfully"}
+    raise HTTPException(status_code=404, detail=f"Book with id: {book_id} not found")
